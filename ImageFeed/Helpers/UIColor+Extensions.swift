a62fe751e7c9ccec6c095e5e44c6ae0548bd4607
dmirain <dmirain@ya.ru>
2023-08-06 00:19:22 +0300

    Добавил градиет под дату

---
+++ ImageFeed/Helpers/UIColor+Extensions.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+extension UIColor {
+    static var ypBlack = UIColor(named: "YP Black")!
+    static var ypBlackA20 = UIColor(named: "YP Black Alpha 20")!
+    static var ypGray = UIColor(named: "YP Gray")!
+    static var ypBackground = UIColor(named: "YP Background")!
+    static var ypWhite = UIColor(named: "YP White")!
+    static var ypWhiteA50 = UIColor(named: "YP White Alpha 50")!
+    static var ypRed = UIColor(named: "YP Red")!
+    static var ypGreen = UIColor(named: "YP Blue")!
+}
--- ImageFeed/Presentation/ImagesListCell.swift
+++ ImageFeed/Presentation/ImagesListCell.swift
@@ -2,9 +2,12 @@ import UIKit
 
 final class ImagesListCell: UITableViewCell {
     static let reuseIdentifier = "ImagesListCell"
+    
+    private var gradientInited = false
 
     @IBOutlet private weak var dateLabel: UILabel!
     @IBOutlet private weak var likeButon: UIButton!
+    @IBOutlet private weak var gradientView: UIView!
     @IBOutlet private weak var cellImage: UIImageView!
 
     func configureCell(with cellModel: ImageCellModel) {
@@ -13,5 +16,15 @@ final class ImagesListCell: UITableViewCell {
         likeButon.setTitle("", for: .normal)
         likeButon.setImage(image, for: .normal)
         cellImage.image = cellModel.image
+        
+        if !gradientInited {
+            let gradient = CAGradientLayer()
+            gradient.frame = gradientView.bounds
+            gradient.colors = [UIColor.clear.cgColor, UIColor.ypBlackA20.cgColor]
+            gradientView.layer.insertSublayer(gradient, at: 0)
+            gradientView.roundCorners(corners: [.bottomLeft, .bottomRight], radius: 16)
+
+            gradientInited.toggle()
+        }
     }
 }

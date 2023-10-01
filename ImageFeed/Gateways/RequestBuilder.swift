baf5a60a2204c73198e4d2f8dfe0928590272605
dmirain <dmirain@ya.ru>
2023-10-01 16:19:36 +0300

    Увеличил таймаут для медленного инернета

--- ImageFeed/Gateways/RequestBuilder.swift
+++ ImageFeed/Gateways/RequestBuilder.swift
@@ -16,7 +16,7 @@ struct RequestBuilderImpl: RequestBuilder {
         components.path = path
         var request = URLRequest(url: components.url!)
         request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
-        request.timeoutInterval = 2 // seconds
+        request.timeoutInterval = 5 // seconds
         request.httpMethod = "GET"
         return request
     }
